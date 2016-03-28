d762be9f1b8115c41ade41e13700db34a646bf0d
Gabriel Tiburcio <tibuurcio@gmail.com>
2016-03-29 01:09:43 +0900

    added act method in Block class

--- Block.java
+++ Block.java
@@ -37,6 +37,14 @@ public class Block extends Actor
     }
 
     /**
+     * Act - do whatever the test wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() {
+        // Add your action code here.
+    }
+
+    /**
     * Get the world instance for this Block as soon as
     * it is added to the world.
     */
