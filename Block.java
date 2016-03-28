23c9b99cf3d2c9bdca5c4d56f76e573051e8cfc4
Gabriel Tiburcio <tibuurcio@gmail.com>
2016-03-28 23:28:45 +0900

    Part1 sketch preparated

--- Block.java
+++ Block.java
@@ -20,8 +20,6 @@ public class Block extends Actor
     private GreenfootImage bombImage;
     private GreenfootImage starImage;
     private boolean bomb;
-    private boolean turned;
-    private boolean question;
     private int number;
     private int i, j;
 
@@ -32,11 +30,8 @@ public class Block extends Actor
     public Block(int i, int j) {
         blockImage = new GreenfootImage("Block.png");
         bombImage = new GreenfootImage("Bomb.png");
-        starImage = new GreenfootImage("Star.png");
 
         bomb = false;
-        turned = false;
-        question = false;
         number = 0;
 
         this.i = i;
@@ -53,93 +48,6 @@ public class Block extends Actor
         this.game = (Minesweeper) game;
     }
 
-    /**
-     * Check to see if the user is clicking this block and handle the
-     * click by calling the world's methods for sorting the bombs after
-     * the first play, updating the numbers, turning the block, displaying
-     * the question marker and checking if the user won the game.
-     */
-    public void act() {
-        if(Greenfoot.mouseClicked(this)) {
-            int mouse = Greenfoot.getMouseInfo().getButton();
-            if(mouse == 1 && !question) {
-                if(game.firstPlay) {
-                    game.sortBombs(i, j);
-                    game.updateNumbers();
-                    game.firstPlay = false;
-                }
-                turn();
-            }
-            else if(mouse == 3) {
-                putQuestion();
-            }
-            game.checkWin();
-        }
-    }
-
-    /**
-    * Display the question marker in this blocks.
-    */
-    public void putQuestion() {
-        if(!turned && !question) {
-            setImage(starImage);
-            this.question = true;
-            if(bomb) {
-                game.hits++;
-            }
-        } else if(!turned && question) {
-            setImage(blockImage);
-            this.question = false;
-            if(bomb) {
-                game.hits--;
-            }
-        } else if(turned && question) {
-            setImage(new GreenfootImage(String.valueOf(this.number),
-                game.CELLSIZE, Color.BLACK, new Color(0, 0, 0, 0)));
-        }
-    }
-
-    /**
-    * Turn this block after checking if it's not a bomb.
-    */
-    public void turn() {
-        if(bomb) {
-            //explode
-            setImage(bombImage);
-            game.showText("Perdeu", Minesweeper.WIDTH/2, 2);
-            Greenfoot.stop();
-        } else {
-            this.turned = true;
-            if(!question) {
-                GreenfootImage img = new GreenfootImage(String.valueOf(this.number),
-                game.CELLSIZE, Color.BLACK, new Color(0, 0, 0, 0));
-                setImage(img);
-            }
-            if(number == 0) {
-                List<Block> neighbours = getNeighbours(1, true, Block.class);
-                for(Block b : neighbours) {
-                    if(!b.isTurned()) {
-                        b.turn();
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-    * Update the number of this block by counting the amount of bombs arount it.
-    */
-    public void updateNumber() {
-        List<Block> neighbours = getNeighbours(1, true, Block.class);
-        int cont = 0;
-        for(Block b : neighbours) {
-            if(b.bomb) {
-                cont++;
-            }
-        }
-        number = cont;
-    }
-
     public void setAsBomb() {
         this.bomb = true;
     }
@@ -150,14 +58,6 @@ public class Block extends Actor
 
     public boolean isBomb() {
         return this.bomb;
-    }
-
-    public boolean isTurned() {
-        return this.turned;
-    }
-
-    public boolean isQuestion() {
-        return this.question;
     }
 
     public void setBombImage() {
--- Minesweeper.java
+++ Minesweeper.java
@@ -12,7 +12,6 @@ public class Minesweeper extends World {
 
     private int fieldSize;
     private int bombs;
-    private int questions;
 
     public int hits;
     public boolean firstPlay;
@@ -27,7 +26,6 @@ public class Minesweeper extends World {
         super(WIDTH, HEIGHT, CELLSIZE, false);
 
         firstPlay = true;
-        questions = 0;
         hits = 0;
         fieldSize = 10;
         bombs = 10;
@@ -41,146 +39,6 @@ public class Minesweeper extends World {
                 addObject(field[i][j], k, l);
             }
         }
-    }
-
-    /**
-    * Check wether the user has won the game.
-    */
-    public void checkWin() {
-        questions = countQuestions();
-        if(questions == bombs && hits == bombs) {
-            showText("Ganhou", WIDTH/2, 2);
-            showBombs();
-            Greenfoot.stop();
-        }
-
-        int unturned = countUnturned();
-        if(unturned == bombs) {
-            showText("Ganhou", WIDTH/2, 2);
-            showBombs();
-            Greenfoot.stop();
-        }
-    }
-
-    /**
-    * Used for showing the bombs location in case the user has won the game.
-    */
-    public void showBombs() {
-        for(int i = 0; i < fieldSize; i++) {
-            for(int j = 0; j < fieldSize; j++) {
-                if(field[i][j].isBomb()) {
-                    field[i][j].setBombImage();
-                }
-            }
-        }
-    }
-
-    /**
-    * Count the number of question marks in the field to see if the user
-    * has mark enough bombs to win the game.
-    */
-    public int countQuestions() {
-        int count = 0;
-        for(int i = 0; i < fieldSize; i++) {
-            for(int j = 0; j < fieldSize; j++) {
-                if(field[i][j].isQuestion()) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    /**
-    * Count the number of field Blocks that are unturned to see if the user
-    * has won the game.
-    */
-    public int countUnturned() {
-        int count = 0;
-        for(int i = 0; i < fieldSize; i++) {
-            for(int j = 0; j < fieldSize; j++) {
-                if(!field[i][j].isTurned()) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    /**
-    * Update the numbers in each Block based on the number of bombs around it.
-    */
-    public void updateNumbers() {
-        for(int i = 0; i < fieldSize; i++) {
-            for(int j = 0; j < fieldSize; j++) {
-                field[i][j].updateNumber();
-            }
-        }
-    }
-
-    /**
-    * Sort the location for the bombs in the field after the first play
-    * from the user.
-    */
-    public void sortBombs(int i, int j) {
-        int v1[] = new int[bombs];
-        int v2[] = new int[bombs];
-
-        sortBombLocations(v1, v2);
-        if(field[i][j].isBomb()) {
-            field[i][j].removeBomb();
-            sortOneBomb();
-        }
-    }
-
-    /**
-    * Sort the location for the bombs two int[] and scrambling them to
-    * be used as positions i and j in the field matrix.
-    */
-    public void sortBombLocations(int v1[], int v2[]) {
-        for(int i = 0, j = 0; i < bombs; i++, j++) {
-            if(j == fieldSize) j = 0;
-            v1[i] = j;
-        }
-
-        for(int i = 0; i < bombs; i++) {
-            int r = Greenfoot.getRandomNumber(bombs);
-            int aux = v1[i];
-            v1[i] = v1[r];
-            v1[r] = aux;
-        }
-
-        for(int i = 0, j = 0; i < bombs; i++, j++) {
-            if(j == fieldSize) j = 0;
-            v2[i] = j;
-        }
-
-        for(int i = 0; i < bombs; i++) {
-            int r = Greenfoot.getRandomNumber(bombs);
-            int aux = v2[i];
-            v2[i] = v2[r];
-            v2[r] = aux;
-        }
-
-        for(int i = 0; i < bombs; i++) {
-            field[v1[i]][v2[i]].setAsBomb();
-        }
-    }
-
-    /**
-    * Sort the location for one single bomb in case them
-    * {@link sortBombLocations(int v1[], int v2[]) sortBombLocations} method
-    * has sorted the position in the field where the user has done it's
-    * first play.
-    */
-    public void sortOneBomb() {
-        int r1, r2;
-        do {
-            r1 = Greenfoot.getRandomNumber(fieldSize);
-            r2 = Greenfoot.getRandomNumber(fieldSize);
-        } while(!field[r1][r2].isBomb());
-
-        field[r1][r2].setAsBomb();
     }
 
     public int getFieldSize() {
